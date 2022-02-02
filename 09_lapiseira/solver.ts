91874b423ff055ff76cbdef3840615858e495bea
Weslem <weslem.wrs@gmail.com>
2022-02-02 19:36:32 -0300

    Organização do repositório para apresentação

--- 09_lapiseira/solver.ts
+++ 09_lapiseira/solver.ts
@@ -32,62 +32,89 @@ class Grafite{
 }
 
 class Lapiseira {
-    calibre: number;
-    private grafite: Grafite | null;
+    private calibre: number;
+    private tambor: Array<Grafite>;
 
     constructor(calibre: number){
         this.calibre = calibre;
-        this.grafite = null;
+        this.tambor = [];
     }
 
-    setGrafite(grafite: Grafite): boolean{
-        if(this.grafite != null){
+    public setGrafite(grafite: Grafite): boolean{
+        if(this.tambor.length > 5){
             console.log("A lapiseira já possui um grafite");
-            return;
+            return false;
         }
         if(grafite.calibre != this.calibre){
             console.log("O grafite não é compativel com a lapiseira");
-            return;
+            return false;
         }
-        this.grafite = grafite;
+        if(grafite.calibre != this.calibre){
+            console.log("O grafite não é compativel com a lapiseira");
+            return false;
+        }
+
+        this.tambor.push(grafite);
         return true;
     }
 
     removerGrafite(): Grafite | null{
-        if(this.grafite == null){
+        if(this.tambor == null){
             console.log("A lapiseira não possui um grafite");
             return null;
         }
 
-        let grafite = this.grafite;
-        this.grafite = null;
+        let grafite = this.tambor.shift();
+        if(grafite == null){
+            return null;
+        }
         return grafite;
     }
 
-    escrever(folhas: number): boolean {
+    escrever(folhas: number): boolean | void {
         //verificar se existe grafite
-        if (this.grafite == null) {
+        if (this.tambor.length == 0) {
             console.log("A lapiseira não possui um grafite");
             return false;
         }
-        let gasto = this.grafite.gastoPorFolha() * folhas;
-        if (gasto <= this.grafite.tamanho) {
-            console.log("Escrita concluida");
-            this.grafite.tamanho -= gasto;
-        } else {
-            let realizado = this.grafite.tamanho / this.grafite.gastoPorFolha()
+        let gasto = this.tambor[0].gastoPorFolha() * folhas;
+
+        if (gasto <= this.tambor[0].tamanho) {
+            console.log("Escrita concluída.");
+            this.tambor[0].tamanho -= gasto;
+            if (this.tambor[0].tamanho == 0){
+                this.removerGrafite();
+                console.log("Tiramos o grafite acabado.")
+                return true;
+            }
+            return true;
+        } else if (this.tambor[0].tamanho != 0){
+            let realizado = this.tambor[0].tamanho / this.tambor[0].gastoPorFolha();
             console.log("Escrita parcial: " + realizado + " folhas");
-            this.grafite.tamanho = 0;
+            this.tambor[0].tamanho = 0;
+            if (this.tambor[0].tamanho == 0){
+                this.removerGrafite();
+                console.log("Tiramos o grafite acabado.")
+                return true;
+            }
+            return true;
         }
-        if (this.grafite.tamanho == 0) {
-            this.grafite = null;
-        }        
+    }
+
+    toString(){
+        return 
     }
 }
 
 let pentel = new Lapiseira(0.5);
 pentel.setGrafite(new Grafite(0.5, "HB", 40));
+pentel.setGrafite(new Grafite(0.5, "2B", 80));
+pentel.setGrafite(new Grafite(0.5, "4B", 50));
+console.log(pentel);
 
-pentel.escrever(10);
 pentel.escrever(40);
+pentel.escrever(10);
+pentel.escrever(50);
+pentel.escrever(60);
+pentel.escrever(70);
 console.log(pentel);
